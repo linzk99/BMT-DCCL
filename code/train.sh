2baea31aaa21108c1ceee77e6957ed4d2e9f893d
linzk99 <1183621933@qq.com>
2024-11-14 11:45:10 +0800

    delete

--- code/train.sh
+++
@@ -1,7 +0,0 @@
-# ACDC  
-CUDA_VISIBLE_DEVICES=0 python -u train_BMT_DCCL.py --exp ACDC/BMT_DCCL --labeled_num 7 --cutmix_prob 0.75 --root_path ../data/ACDC --num_classes 4
-
-# PROMISE
-CUDA_VISIBLE_DEVICES=0 python -u train_BMT_DCCL.py --exp Prostate/BMT_DCCL --labeled_num 7 --cutmix_prob 1 --root_path ../data/Prostate --num_classes 2
-
-
